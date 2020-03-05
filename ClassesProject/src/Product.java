c94dcc418cd07ce1ae8f152157413d0c9bd4e330
NathanaelGastelum <34591895+NathanaelGastelum@users.noreply.github.com>
2020-03-04 23:44:21 -0800

    Added javadoc comments

--- ClassesProject/src/Product.java
+++ ClassesProject/src/Product.java
@@ -1,4 +1,3 @@
-
 public class Product {
 
     private String name;
@@ -13,34 +12,66 @@ public class Product {
 
     }
 
+    /**
+     * Gets Product Name
+     * @return Product name as a String
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets Product Name
+     * @param name New String
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Sets Product price
+     * @param price New double
+     */
     public void setPrice(double price) {
         this.price = price;
     }
 
+    /**
+     * Gets Product price
+     * @return Product price as a double
+     */
     public double getPrice() {
         return price;
     }
 
+    /**
+     * Gets Product quantity
+     * @return Product price as an int
+     */
     public int getQuantity() {
         return quantity;
     }
 
+    /**
+     * Sets Product amount
+     * @param quantity New int
+     */
     public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
 
+    /**
+     * Lists Product name and price
+     * @return Product name and price as a String
+     */
     public String itemListing() {
         return name + " @ " + String.format("%.2f", getPrice());
     }
 
+    /**
+     * Gets Product details
+     * @return Product name, price, and quantity as a String
+     */
     @Override
     public String toString() {
         return getName() + " @ " + String.format("%.2f", getPrice()) + ", " + getQuantity() + " left";
